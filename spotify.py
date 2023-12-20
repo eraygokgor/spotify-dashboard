872282d9f780bdf1f785054ae8cee4b90995af7e
Eray Gökgör <gokgoreray@gmail.com>
2023-12-20 22:22:08 +0300

    update work

--- spotify.py
+++ spotify.py
@@ -1,14 +1,24 @@
-from flask import Flask, redirect, request, make_response
+from flask import Flask, redirect, request
+from urllib.parse import urlencode
 from credentials import CLIENT_ID, CLIENT_SECRET
+from functions import generate_random_string, base64_encoder, sha256_hash
 import requests
-import random
-import string
-import base64 as b64
 
+# Spotify API credentials
 client_id = CLIENT_ID
 client_secret = CLIENT_SECRET
 redirect_uri = 'http://127.0.0.1:5000/callback'
 
+# Spotify API endpoints
+SPOTIFY_API_URL = 'https://api.spotify.com/v1/'
+SPOTIFY_AUTH_URL = 'https://accounts.spotify.com/authorize'
+SPOTIFY_TOKEN_URL = 'https://accounts.spotify.com/api/token'
+
+# Scopes required by your application
+SPOTIFY_SCOPES = ['user-read-private', 'user-read-email']
+
+# Local storage
+local_storage = dict()
 
 app = Flask(__name__)
 
@@ -21,40 +31,50 @@ def index():
 
 @app.route('/login')
 def authorize():
-    print('login request received')
-    scope = 'user-read-private user-read-email'
-    state_key = ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(16))
+    code_verifier = generate_random_string(64)
+    # Store the code verifier in local storage to use later to exchange for a token
+    local_storage['code_verifier'] = code_verifier
+    code_challenge = base64_encoder(sha256_hash(code_verifier))
 
-    authorize_url = 'https://accounts.spotify.com/authorize?'
-    parameters = 'response_type=code&' + \
-                 f'client_id={client_id}&' + \
-                 f'scope={scope}&' + \
-                 f'redirect_uri={redirect_uri}&' + \
-                 f'state={state_key}'
+    params = {
+        'client_id': client_id,
+        'response_type': 'code',
+        'redirect_uri': redirect_uri,
+        'scope': ' '.join(SPOTIFY_SCOPES),
+        'code_challenge_method': 'S256',
+        'code_challenge': code_challenge,
+    }
 
-    response = make_response(redirect(authorize_url + parameters))
-    return response
+    auth_url = f'{SPOTIFY_AUTH_URL}?{urlencode(params)}'
+    return redirect(auth_url)
 
 
 @app.route('/callback')
 def callback():
-    print('callback request received')
-    print('---', request.args.get('state'))
-    if request.args.get('state') is None:
-        print('state is None')
-        return redirect('/')
+    code = request.args.get('code')
+
+    if request.args.get('error'):
+        return request.args.get('error')
     else:
-        url = 'https://accounts.spotify.com/api/token'
-        code = request.args.get('code')
-        parameters = {
+        params = {
             'grant_type': 'authorization_code',
             'code': code,
-            'redirect_uri': redirect_uri
-        }
-        headers = {
-            'content_type': 'application/x-www-form-urlencoded',
-            'Authorization': 'Basic ' + b64.b64encode((client_id + ':' + client_secret).encode('ascii')).decode('ascii')
+            'redirect_uri': redirect_uri,
+            'client_id': client_id,
+            'code_verifier': local_storage['code_verifier'], # Retrieve the code verifier from local storage
         }
 
-        response = requests.post(url, data=parameters, headers=headers)
-    return response.text
+        headers = {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        }
+
+        response = requests.post(SPOTIFY_TOKEN_URL, data=params, headers=headers)
+        access_token_data = response.json()
+
+        headers_user = {
+            'Authorization': f'Bearer {access_token_data["access_token"]}'
+        }
+        response_user = requests.get(SPOTIFY_API_URL + 'me', headers=headers_user)
+        user_data = response_user.json()
+
+        return user_data
