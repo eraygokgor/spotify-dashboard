77921186435a752b727e52184c6905420e7f23b4
Eray Gökgör <gokgoreray@gmail.com>
2023-12-29 22:14:43 +0300

    update

--- spotify.py
+++ spotify.py
@@ -1,6 +1,6 @@
 from flask import Flask, redirect, request, render_template, session
 from urllib.parse import urlencode
-from crendentials import CLIENT_ID, CLIENT_SECRET
+from credentials import CLIENT_ID, CLIENT_SECRET
 from functions import generate_random_string, base64_encoder, sha256_hash
 import requests
 
